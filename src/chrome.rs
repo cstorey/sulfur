55d9fb2c2b3b4572258945fc12b23b52bd68bb4c
Ceri Storey <cez@necrofish.org.uk>
2018-12-22 16:15:54 +0000

    Start from port 4444 onwards.

--- src/chrome.rs
+++ src/chrome.rs
@@ -118,7 +118,25 @@ impl Config {
 }
 
 fn unused_port_no() -> Result<u16, Error> {
-    let a = SocketAddr::from(([0, 0, 0, 0], 0));
-    let l = TcpListener::bind(a).context("Binding to ephemeral port")?;
-    Ok(l.local_addr().context("Listener local port")?.port())
+    let start_port = 4444u16;
+    for port in start_port.. {
+        let a = SocketAddr::from(([127, 0, 0, 1], port));
+        debug!("Trying to bind to address: {:?}", a);
+        if let Some(l) = TcpListener::bind(a).map(Some)
+            .or_else(|e| {
+                if e.kind() == std::io::ErrorKind::AddrInUse {
+                    info!("Retrying");
+                    Ok(None)
+                } else {
+                    warn!("Error binding to {:?}; kind:{:?}; {:?}", a, e.kind(), e);
+                    Err(e)
+                }
+            })
+            .context("Binding to ephemeral port")? {
+                let addr = l.local_addr().context("Listener local port")?;
+                info!("Available: {}", addr);
+                return Ok(addr.port())
+            }
+    }
+    bail!("Could not find un-used port from {}..", start_port)
 }
