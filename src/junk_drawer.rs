c313c4f576629134505d6053c15a77f05df8b095
Ceri Storey <cez@necrofish.org.uk>
2018-12-22 17:02:11 +0000

    Avoid races in port allocation.

--- src/junk_drawer.rs
+++ src/junk_drawer.rs
@@ -1,12 +1,21 @@
 use failure::Error;
 use std::net::{SocketAddr, TcpListener};
+use std::sync::atomic::*;
 
 use failure::ResultExt;
 
+// We do this shenanigans to (hopefully) avoid a race condition where
+// two threads test that a port is "free" one after the other, but before
+// either is able to start it's driver.
+static PORT: AtomicUsize = ATOMIC_USIZE_INIT;
 
 pub fn unused_port_no() -> Result<u16, Error> {
     let start_port = 4444u16;
-    for port in start_port.. {
+    loop {
+        let off = PORT.fetch_add(1, Ordering::SeqCst);
+        let port = start_port.checked_add(off as u16).ok_or_else(|| {
+            failure::err_msg("Allocated more ports than we have namespace for?")
+        })?;
         let a = SocketAddr::from(([127, 0, 0, 1], port));
         debug!("Trying to bind to address: {:?}", a);
         if let Some(l) = TcpListener::bind(a)
@@ -25,5 +34,4 @@ pub fn unused_port_no() -> Result<u16, Error> {
             return Ok(addr.port());
         }
     }
-    bail!("Could not find un-used port from {}..", start_port)
 }
